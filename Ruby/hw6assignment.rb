a85fc4b496304f4fbd7b05632f540d89779a8b04
jin.xiao <jin.xiao@immomo.com>
2017-07-27 20:52:45 +0800

    finish hw6

--- Ruby/hw6assignment.rb
+++ Ruby/hw6assignment.rb
@@ -5,18 +5,103 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  # class array holding all the pieces and their rotations
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [-1, -1]]),
+               [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+               [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+               rotations([[0, 0], [1, 0], [0, -1]])]
 
   # your enhancements here
+
+  # class method to choose the next piece
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
+
+  def self.cheat_piece (board)
+    MyPiece.new([[0, 0]], board)
+  end
 
 end
 
 class MyBoard < Board
   # your enhancements here
 
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+    @cheated = false
+  end
+
+  # gets the next piece
+  def next_piece
+    if @cheated
+      @current_block = MyPiece.cheat_piece(self)
+      @score -= 100
+      @cheated = false
+    else
+      @current_block = MyPiece.next_piece(self)
+    end
+    @current_pos = nil
+  end
+
+  # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    locations.each_index{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+
+  def rotate_180
+  	if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+    draw
+  end
+
+  def cheat
+    if @score > 100
+      @cheated = true
+    end
+  end
+
 end
 
 class MyTetris < Tetris
   # your enhancements here
+
+    # creates a canvas and the board that interacts with it
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings
+  	super
+  	@root.bind('u', proc {@board.rotate_180})
+    @root.bind('c', proc {@board.cheat})
+  end
 
 end
 
